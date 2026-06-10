dd6168eee53b592f0eed06f2af728c0dcd3cde04
M33V <md33vr@gmail.com>
2026-06-11 00:37:07 +0300

    добавлен общий метод

--- cogs/art_grabber.py
+++ cogs/art_grabber.py
@@ -12,11 +12,14 @@ import requests
 
 class Artgrabber(commands.Cog, name = "art_grabber"):
     log = logging.getLogger(__name__)
-    BASE_URL = "https://danbooru.donmai.us/posts.json"
+    headers = {
+        "user-agent": "prop.spell"
+    }
+    BASE_URL = "https://danbooru.donmai.us"
     def __init__(self, bot) -> None:
         self.bot = bot
-    
-
+        self.r = requests.Session()
+        self.r.headers.update(self.headers)
     @app_commands.command(
         name = "random_art",
         description= "Give a random art, from all resourses"
@@ -24,6 +27,7 @@ class Artgrabber(commands.Cog, name = "art_grabber"):
     @app_commands.guilds(GUILD)
     async def random_art(self, interaction: discord.Interaction,tags: str) -> None:
         DEFAULT_LIMIT = 1
+        ex_url = "/posts.json"
         fx_tags = re.sub(r' ', '_', tags).replace(":", " ")
         await interaction.response.defer()
         print(fx_tags)
@@ -34,37 +38,31 @@ class Artgrabber(commands.Cog, name = "art_grabber"):
             "rating": "g"
             
             }
-        headers = {
-        "user-agent": "prop.spell"
-    }
-        with requests.Session() as r:
-            r.headers.update(headers)
- 
-            try:
-                response = r.get(self.BASE_URL, params= params)
-                response.raise_for_status()
-                data = response.json()
-            
-                if not data:
-                    await interaction.followup.send("Empty page")
-                    return None
+        data = self.get_data(ex_url, params = params)
+               
+        if not data:
+            await interaction.followup.send("Empty page")
+            return None
                 
-                post = data[0] 
-                 
-                RATING = post["rating"]               
-                print(RATING) 
-                image_url = post["file_url"]
-                print(image_url)
+        post = data[0] 
+        image_url = post["file_url"]
+        print(image_url)
             
-                await interaction.followup.send(image_url)   
-                    
-                    
-                    
-            except requests.RequestException as e:
-                print(f"request error: {e}")
-                
+        await interaction.followup.send(image_url)   
+    
+
+    def get_data(self,ex_url: str, params: dict):
+        url = self.BASE_URL + ex_url
+        try:
+            response = self.r.get(url, params = params)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException as e:
+            print(f"request error: {e}")
+        
 
 async def setup(bot) -> None:
     await bot.add_cog(Artgrabber(bot))
 
-
+# Нужна команда на реально рандомный арт
+# Команда с рандомным автором + о нем
